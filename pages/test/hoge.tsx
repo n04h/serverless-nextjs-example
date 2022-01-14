7368059607a6b81cb64622ebafa508c8b9d01fda
n04h <contact@n04h.jp>
2022-01-14 23:45:36 +0900

    chore: デプロイとか色々検証

---
+++ pages/test/hoge.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Test hoge</title>
+      </Head>
+
+      <main>
+        <h1 className="title">
+          Test hoge
+        </h1>
+      </main>
+    </div>
+  )
+}
